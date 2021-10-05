a20f86281e6e4912f2dd8e21cc093b6dd6f8f03c
ipxz-p <64070193@it.kmitl.ac.th>
2021-10-05 21:34:24 +0700

    video

--- website/views.py
+++ website/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "frontend/index.htm")
+    return render(request, "frontend/main_page.htm")
 def test(request):
     return render(request, "frontend/test.htm")
