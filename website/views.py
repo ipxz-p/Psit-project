9485f5071b2f0615cbe27713d8277f1af364d6e5
ipxz-p <88420839+ipxz-p@users.noreply.github.com>
2021-12-09 15:34:34 +0700

    Delete website directory

--- website/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return render(request, "frontend/main_page.htm")
-def test(request):
-    return render(request, "frontend/test.htm")
